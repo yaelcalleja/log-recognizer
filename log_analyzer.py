fc6d52bc51e3c54dc275277cfb63a38a6d1ddc13
badger <yael.calleja321@gmail.com>
2025-10-27 16:09:00 -0600

    Add a function to read the .log file

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,7 +1,24 @@
 import re
-import auth.log as x
+
+sampleLog = "auth.log"
 
 
+def read_the_text():
+    print(f"--------Reading the file: {sampleLog}----------")
+
+    try:
+        with open(sampleLog, "r") as log:
+            for line in log:
+                print(line.strip())
+
+    except FileNotFoundError:
+        print("* ERROR You must have the file on the same carpet of the scipt!!")
+
+    print("End of file")
+
+
+"""
 def log_reconigzer():
     re.search("Failed")
     return
+"""
