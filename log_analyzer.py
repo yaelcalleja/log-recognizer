567022045f79938d5607ffd864fd5debf4a2350c
badger <yael.calleja321@gmail.com>
2025-10-29 16:06:18 -0600

    Main file of the script

--- log_analyzer.py
+++ log_analyzer.py
@@ -3,16 +3,21 @@ import re
 sampleLog = "auth.log"
 
 
-def read_the_text():
+def search_in_text():
     print(f"--------Reading the file: {sampleLog}----------")
 
     try:
         with open(sampleLog, "r") as log:
             for line in log:
-                print(line.strip())
+                DateTime = re.findall(r'^[A-Z]{1}[a-z]{2} [1-30]+ ([00-60].){2}.[00-60]', line, flags=re.MULTILINE)
+                Status = re.search()
+                User = re.search()
+                Ip = re.search()
+                Port = re.search()
+                Service = re.findall()
 
     except FileNotFoundError:
-        print("* ERROR You must have the file on the same carpet of the scipt!!")
+        print("[/] ERROR You must have the file on the same carpet of the scipt!!")
 
     print("End of file")
 
