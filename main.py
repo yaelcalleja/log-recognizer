1197a5e36d2bde610c1d5bbdb6632efa4670aa9d
badger <yael.calleja321@gmail.com>
2025-11-16 10:49:46 -0600

    Main file cleaned, class completed

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 import re
-from reader import search_in_text as look
+from reader import LogEntry as 
 
 
 def main():
-    print("------------------------------------")
-    print("[*] Welcome to the log analyzer [*]")
-    print("------------------------------------")
-    flag = input(
-        "[?] Do you want me to analyze your log? \n[Press any key to continue or \"e\" to exit]: "
-    )
-    if flag == "e":
-        flag = False
-    else:
-        flag = True
-    while flag:
-        print("[*] --------------------------------------------------------------------------------- [*]")
-        print("[*] This script will create a dictionary with all the data... Pls wait a few seconds")
+
+    return
